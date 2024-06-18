313313b3d98150a005105411db5672692488f207
kenneth <zhaho.ros@gmail.com>
2024-06-18 11:06:05 +0000

    detection for subnet

--- toggle-external-traffic.sh
+++ toggle-external-traffic.sh
@@ -1,29 +1,35 @@
 #!/bin/bash
 
-# Get the third octet of the local subnet from the IP address
-SUBNET=$(ip a | grep -oP 'inet \K192\.168\.\d+' | awk -F '.' '{print $3}')
+# Function to extract subnet from IP address
+get_subnet() {
+    local subnet
+    subnet=$(echo "$1" | awk -F '.' '{print $3}')
+    echo "$subnet"
+}
+
+# Determine local subnet
+SUBNET=$(ip a | grep -oP 'inet \K(192\.168|10\.)\.\d+' | awk '{print $2}')
 
 # Check if there is a rule to drop incoming traffic
 STATE=$(sudo iptables -S | grep 'INPUT -j DROP')
 
 # If no rule to drop incoming traffic exists, set up the rules to block internet access
-if [ -z "$STATE" ]
-then
-    # Allow incoming traffic from the local subnet
-    sudo iptables -A INPUT -s 192.168.$SUBNET.0/24 -j ACCEPT
+if [ -z "$STATE" ]; then
+    # Allow incoming traffic from the detected subnet
+    sudo iptables -A INPUT -s "$SUBNET".0/24 -j ACCEPT
     # Allow incoming traffic on the loopback interface
     sudo iptables -A INPUT -i lo -j ACCEPT
     # Drop all other incoming traffic
     sudo iptables -A INPUT -j DROP
 
-    # Allow outgoing traffic to the local subnet
-    sudo iptables -A OUTPUT -d 192.168.$SUBNET.0/24 -j ACCEPT
+    # Allow outgoing traffic to the detected subnet
+    sudo iptables -A OUTPUT -d "$SUBNET".0/24 -j ACCEPT
     # Allow outgoing traffic on the loopback interface
     sudo iptables -A OUTPUT -o lo -j ACCEPT
     # Drop all other outgoing traffic
     sudo iptables -A OUTPUT -j DROP
 
-    echo "Subnet: 192.168.$SUBNET.0/24"
+    echo "Subnet: $SUBNET.0/24"
     echo "Internet Banned"
 
 # If a rule to drop incoming traffic exists, remove the rules to allow internet access
@@ -35,3 +41,4 @@ else
 
     echo "Internet Open"
 fi
+
