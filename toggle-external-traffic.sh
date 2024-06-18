6258b5338e5819bbcd0d84855d9e04421d1d688c
Kenneth Ros <31766495+zhaho@users.noreply.github.com>
2024-06-18 13:38:23 +0200

    Update toggle-external-traffic.sh

--- toggle-external-traffic.sh
+++ toggle-external-traffic.sh
@@ -2,8 +2,12 @@
 
 # Function to get the local subnet dynamically
 get_local_subnet() {
-    local ip=$(ip route | awk '/default/ { print $3 }')
-    local subnet=$(ip route | awk -v ip="$ip" '$1 == ip { print $1 }')
+    # Extract all IPv4 addresses with global scope
+    local addresses=$(ip -o -f inet addr show | awk '/scope global/ {print $4}')
+
+    # Extract subnet from the first address (assuming they are on the same subnet)
+    local subnet=$(echo "$addresses" | head -n 1 | cut -d'/' -f1)
+
     echo "${subnet}"
 }
 
@@ -21,21 +25,21 @@ STATE=$(sudo iptables -S | grep 'INPUT -j DROP')
 # If no rule to drop incoming traffic exists, set up the rules to block internet access
 if [ -z "$STATE" ]; then
     # Allow incoming traffic from the local subnet
-    sudo iptables -A INPUT -s "$LOCAL_SUBNET" -j ACCEPT
+    sudo iptables -A INPUT -s "$LOCAL_SUBNET"/24 -j ACCEPT  # Adjust subnet mask as necessary
     # Allow incoming traffic on the loopback interface
     sudo iptables -A INPUT -i lo -j ACCEPT
     # Drop all other incoming traffic
     sudo iptables -A INPUT -j DROP
 
     # Allow outgoing traffic to the local subnet
-    sudo iptables -A OUTPUT -d "$LOCAL_SUBNET" -j ACCEPT
+    sudo iptables -A OUTPUT -d "$LOCAL_SUBNET"/24 -j ACCEPT  # Adjust subnet mask as necessary
     # Allow outgoing traffic on the loopback interface
     sudo iptables -A OUTPUT -o lo -j ACCEPT
     # Drop all other outgoing traffic
     sudo iptables -A OUTPUT -j DROP
 
-    echo "Subnet: $LOCAL_SUBNET"
-    echo "Internet Closed"
+    echo "Subnet based on IP: $LOCAL_SUBNET/24"  # Adjust subnet mask as necessary
+    echo "Internet Banned"
 
 # If a rule to drop incoming traffic exists, remove the rules to allow internet access
 else
