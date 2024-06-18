07807f21885fef5c59f81e41e9be8a7dfee38bf7
Kenneth Ros <31766495+zhaho@users.noreply.github.com>
2024-06-18 13:12:44 +0200

    Update toggle-external-traffic.sh

--- toggle-external-traffic.sh
+++ toggle-external-traffic.sh
@@ -1,35 +1,40 @@
 #!/bin/bash
 
-# Function to extract subnet from IP address
-get_subnet() {
-    local subnet
-    subnet=$(echo "$1" | awk -F '.' '{print $3}')
-    echo "$subnet"
+# Function to get the local subnet dynamically
+get_local_subnet() {
+    local ip=$(ip route | awk '/default/ { print $3 }')
+    local subnet=$(ip route | awk -v ip="$ip" '$1 == ip { print $1 }')
+    echo "${subnet}"
 }
 
-# Determine local subnet
-SUBNET=$(ip a | grep -oP 'inet \K(192\.168|10\.)\.\d+' | awk '{print $2}')
+# Detect the local subnet
+LOCAL_SUBNET=$(get_local_subnet)
+
+if [ -z "$LOCAL_SUBNET" ]; then
+    echo "Failed to detect local subnet."
+    exit 1
+fi
 
 # Check if there is a rule to drop incoming traffic
 STATE=$(sudo iptables -S | grep 'INPUT -j DROP')
 
 # If no rule to drop incoming traffic exists, set up the rules to block internet access
 if [ -z "$STATE" ]; then
-    # Allow incoming traffic from the detected subnet
-    sudo iptables -A INPUT -s "$SUBNET".0/24 -j ACCEPT
+    # Allow incoming traffic from the local subnet
+    sudo iptables -A INPUT -s "$LOCAL_SUBNET" -j ACCEPT
     # Allow incoming traffic on the loopback interface
     sudo iptables -A INPUT -i lo -j ACCEPT
     # Drop all other incoming traffic
     sudo iptables -A INPUT -j DROP
 
-    # Allow outgoing traffic to the detected subnet
-    sudo iptables -A OUTPUT -d "$SUBNET".0/24 -j ACCEPT
+    # Allow outgoing traffic to the local subnet
+    sudo iptables -A OUTPUT -d "$LOCAL_SUBNET" -j ACCEPT
     # Allow outgoing traffic on the loopback interface
     sudo iptables -A OUTPUT -o lo -j ACCEPT
     # Drop all other outgoing traffic
     sudo iptables -A OUTPUT -j DROP
 
-    echo "Subnet: $SUBNET.0/24"
+    echo "Subnet: $LOCAL_SUBNET"
     echo "Internet Banned"
 
 # If a rule to drop incoming traffic exists, remove the rules to allow internet access
@@ -41,4 +46,3 @@ else
 
     echo "Internet Open"
 fi
-
